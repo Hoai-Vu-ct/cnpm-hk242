10fdf2c4c2d9edc7dc1eb833bb74040f4c1ecf8a
Eijun <eijuntodai@gmail.com>
2025-05-08 20:38:22 +0700

    cap nhap them Session cho BE

--- BE/server.js
+++ BE/server.js
@@ -1,10 +1,22 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(session({
+    secret: 'abcxyz',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+      secure: false, 
+      maxAge: 30 * 24 * 60 * 60 * 1000
+    } 
+  }));
 
 // Automatic cron jobs
 const { startReminderJob } = require('./src/jobs/reminderJob.js');
--- BE/src/controllers/AuthController.js
+++ BE/src/controllers/AuthController.js
@@ -2,6 +2,7 @@ const AuthService = require('../service/AuthService')
 
 class AuthController {
     check = async (req, res) => {
+        
         try {
             const result = await AuthService.check(req);
             return res.status(200).send(result);
